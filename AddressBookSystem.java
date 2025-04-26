d87370bb19d587d7932e8f1ba05e01ee8d00fc3d
vinayg2509 <vinayg2509@gmail.com>
2025-04-26 16:11:25 +0530

    [Vinay]Implemented : Implemented a functionality to add the multiple contact details of a person by used a collection class

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -5,43 +5,50 @@ import java.util.Scanner;
 class AddressBookSystem {
 
 	public static void main(String[] args) {
-
 		Scanner scanner = new Scanner(System.in);
 		AddressBook addressBook = new AddressBook();
 		System.out.println("Welcome to Address Book program");
-		System.out.print("Enter First Name: ");
-		String firstName = scanner.nextLine();
+		boolean addMore = true;
+		while (addMore) {
+			System.out.print("Enter First Name: ");
+			String firstName = scanner.nextLine();
 
-		System.out.print("Enter Last Name: ");
-		String lastName = scanner.nextLine();
+			System.out.print("Enter Last Name: ");
+			String lastName = scanner.nextLine();
 
-		System.out.print("Enter Address: ");
-		String address = scanner.nextLine();
+			System.out.print("Enter Address: ");
+			String address = scanner.nextLine();
 
-		System.out.print("Enter City: ");
-		String city = scanner.nextLine();
+			System.out.print("Enter City: ");
+			String city = scanner.nextLine();
 
-		System.out.print("Enter State: ");
-		String state = scanner.nextLine();
+			System.out.print("Enter State: ");
+			String state = scanner.nextLine();
 
-		System.out.print("Enter ZIP: ");
-		String zip = scanner.nextLine();
+			System.out.print("Enter ZIP: ");
+			String zip = scanner.nextLine();
 
-		System.out.print("Enter Phone Number: ");
-		String phoneNumber = scanner.nextLine();
+			System.out.print("Enter Phone Number: ");
+			String phoneNumber = scanner.nextLine();
 
-		System.out.print("Enter Email: ");
-		String email = scanner.nextLine();
+			System.out.print("Enter Email: ");
+			String email = scanner.nextLine();
 
-		// Create ContactPerson object
-		Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+			// Create ContactPerson object
+			Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
 
+			// Add to AddressBook
+			addressBook.addContact(newContact);
 
-		// Add to AddressBook
-		addressBook.addContact(newContact);
+			System.out.print("Do you want to add another contact? (Y/N): ");
+			String choice = scanner.nextLine();
+			if (!choice.equalsIgnoreCase("y")) {
+				addMore = false;
+			}
+		}
 		System.out.println();
 		// printing that contacts has been successfully added
-		System.out.println("\nContact added successfully! Here's your Address Book:");
+		System.out.println("\n Here's your Address Book with all the contacts:");
 		addressBook.displayContacts();
 
 		// Ask if user wants to edit any contact
