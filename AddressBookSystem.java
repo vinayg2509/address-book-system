ee560fb1fe9fff80333dc833a9b2899599c65573
vinayg2509 <vinayg2509@gmail.com>
2025-04-26 11:18:23 +0530

    [Vinay] Added : created a new class - Contacts by adding the contact details and methods - created a contact object in main class

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -4,6 +4,16 @@ class AddressBookSystem {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book program");
-	}
 
+		Contact contact = new Contact(
+				"Vinay", "G", "1836 Hosmane Main Road", "Bhadravathi", 
+				"Karnataka", "577301", "8123804221", "gvinay240@gmail.com");
+//		Display Contact Details
+		System.out.println(contact);
+		
+		
+		
+	}
 }
+
+
---
+++ Contact.java
@@ -0,0 +1,110 @@
+package addressbooksystem;
+
+public class Contact 
+{
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String email;
+
+	Contact(String firstName, String lastName, String address, String city,
+			String state, String zip, String phoneNumber, String email) {
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setAddress(address);
+		this.setCity(city); 
+		this.setState(state); 
+		this.setZip(zip);
+		this.setAddress(address);
+		this.setEmail(email);
+
+	}
+
+
+
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact{" +
+				"First Name='" + firstName + '\'' +
+				", Last Name='" + lastName + '\'' +
+				", Address='" + address + '\'' +
+				", City='" + city + '\'' +
+				", State='" + state + '\'' +
+				", Zip='" + zip + '\'' +
+				", Phone Number='" + phoneNumber + '\'' +
+				", Email='" + email + '\'' +
+				'}';
+	}
+}
+
